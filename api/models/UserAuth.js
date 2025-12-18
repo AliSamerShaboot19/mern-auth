adfcf636d2f9165a1b9e413f998e32121f7ffa0e
AliSamerShaboot19 <www.ali.s.samer@gmail.com>
2025-12-18 21:27:36 +0300

    create User Model

---
+++ api/models/UserAuth.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const UserAuthSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const UserAuth = mongoose.model("UserAuth", UserAuthSchema);
+
+export default UserAuth;
